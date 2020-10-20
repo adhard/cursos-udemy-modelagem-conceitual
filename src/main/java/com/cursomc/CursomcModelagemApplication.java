09c1c460ac0db8ce1b018e552c6a1e9a3f4887bd
adriano hardtke <adr.hardtke@gmail.com>
2020-10-20 18:49:38 -0300

    repositorio iniciado

---
+++ src/main/java/com/cursomc/CursomcModelagemApplication.java
@@ -0,0 +1,13 @@
+package com.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcModelagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcModelagemApplication.class, args);
+	}
+
+}
