2babdac8b36f4a820a5a467e3440ab24f85742ab
adriano hardtke <adr.hardtke@gmail.com>
2020-10-21 09:19:58 -0300

    Criando operacao de instanciacao - bd iniciar com os dados

--- src/main/java/com/cursomc/CursomcModelagemApplication.java
+++ src/main/java/com/cursomc/CursomcModelagemApplication.java
@@ -1,13 +1,34 @@
 package com.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cursomc.domain.Categoria;
+import com.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcModelagemApplication {
+public class CursomcModelagemApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcModelagemApplication.class, args);
+	}
+	
+	
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
